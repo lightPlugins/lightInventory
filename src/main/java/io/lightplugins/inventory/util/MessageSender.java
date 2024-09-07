89b6cada840d6acb6ce893b8c81d1b218a6dcb63
lightplugins <lightningdesign@outlook.de>
2024-09-08 00:57:05 +0200

    added requirementHandler and ActionHandler

--- src/main/java/io/lightplugins/inventory/util/MessageSender.java
+++ src/main/java/io/lightplugins/inventory/util/MessageSender.java
@@ -3,8 +3,12 @@ import io.lightplugins.inventory.LightMaster;
 import io.lightplugins.inventory.module.LightInv;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
 
 public class MessageSender {
 
@@ -15,4 +19,24 @@ public class MessageSender {
             audience.sendMessage(component);
         });
     }
+
+    public void sendPlayerTitle(String title, String subtitle, int fadeIn, int stay, int fadeOut, Player player) {
+        Bukkit.getScheduler().runTask(LightMaster.instance, () -> {
+            Audience audience = (Audience) player;
+            Component titleComponent = LightMaster.instance.colorTranslation.universalColor(title, player);
+            Component subtitleComponent = LightMaster.instance.colorTranslation.universalColor(subtitle, player);
+            Duration fadeInDuration = Duration.ofMillis(fadeIn * 50L); // Convert ticks to milliseconds
+            Duration stayDuration = Duration.ofMillis(stay * 50L); // Convert ticks to milliseconds
+            Duration fadeOutDuration = Duration.ofMillis(fadeOut * 50L); // Convert ticks to milliseconds
+            Title.Times times = Title.Times.times(fadeInDuration, stayDuration, fadeOutDuration);
+            Title titleObject = Title.title(titleComponent, subtitleComponent, times);
+            audience.showTitle(titleObject);
+        });
+    }
+    public void sendSound(String sound, float volume, float pitch, Player player) {
+        Bukkit.getScheduler().runTask(LightMaster.instance, () -> {
+            player.playSound(player.getLocation(), sound, volume, pitch);
+        });
+    }
+
 }
